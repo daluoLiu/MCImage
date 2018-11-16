33e825c3437846418616986eec6b0304071580d0
大罗 <liuxuan@micous.com>
2018-11-16 21:02:11 +0800

    Add: MCBase.podspec

---
+++ Classes/MCBase.m
@@ -0,0 +1,13 @@
+//
+//  MCBase.m
+//  MCImage
+//
+//  Created by xu'an liu on 2018/11/16.
+//  Copyright © 2018年 bt. All rights reserved.
+//
+
+#import "MCBase.h"
+
+@implementation MCBase
+
+@end
