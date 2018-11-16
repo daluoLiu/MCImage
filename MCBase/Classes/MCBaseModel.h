a34564e00c2dc3b8a015ef51d8abcce2734867ed
大罗 <liuxuan@micous.com>
2018-11-16 21:53:40 +0800

    Modify: MCBase.podspec

---
+++ MCBase/Classes/MCBaseModel.h
@@ -0,0 +1,13 @@
+//
+//  MCBaseModel.h
+//  MCImage
+//
+//  Created by xu'an liu on 2018/11/16.
+//  Copyright © 2018年 bt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MCBaseModel : NSObject
+
+@end
---
+++ MCBase/Classes/MCBaseModel.m
@@ -0,0 +1,13 @@
+//
+//  MCBaseModel.m
+//  MCImage
+//
+//  Created by xu'an liu on 2018/11/16.
+//  Copyright © 2018年 bt. All rights reserved.
+//
+
+#import "MCBaseModel.h"
+
+@implementation MCBaseModel
+
+@end
